818cd67e008693d000e0ef1dc9ade52ab41c5b44
hfuuss <ss@hfuusec.cn>
2016-09-24 15:21:37 +0800

    V1.0

--- ngselectv0.1.2/ngselect.js
+++ ngselectv0.1.2/ngselect.js
@@ -1,33 +1,35 @@
-(function(){
+(function() {
 
 
-angular.module('ssngSelect', []).directive('ssngSelect', function(){
+    angular.module('ssngSelect', []).directive('ssngSelect', function() {
 
-    return {
-        restrict: 'A',
-        scope:{},
-        link: function(scope, element, attrs){
+        return {
+          restrict: 'A',
+          scope: {},
+          link: function(scope, element, attrs) {
 
-          scope.datas =  scope.$parent[attrs['ssngSelect']];
-         
-                     scope.openSlect = false; 
-                                 
-                                    
-                     scope.callback = function(index,text){
-                      scope.index = index+1;
-                      scope.text = text;
-                      alert("你选择了第"+scope.index+"个选项");
-                                                           }
-           
-                                              },
+            scope.datas = scope.$parent[attrs['ssngSelect']];
 
-        template: 
-            '<span ng-click="openSlect=!openSlect" >请选择...</span>' +
-            '<ul ng-show="openSlect">' +
-            '<table><tr ng-repeat="item in datas"><td ng-click="callback($index,item.text)">{{ $index }}:{{ item.text }}</td></tr></table>'+
-            '</ul>'
+            scope.openSlecte = false;
 
-             };
-                                                                    });
+
+            scope.callback = function(index, text) {
+
+            
+            scope.index = index + 1;
+            scope.text = text;
+            alert("你选择了第" + scope.index + "个选项");
+            }
+
+           },
+
+        template:
+          '<span ng-click="openSlecte=!openSlecte" >请选择...</span>' +
+          '<ul ng-show="openSlect">' +
+          '<table><tr ng-repeat="item in datas"><td ng-click="callback($index,item)">{{ $index }}:{{ item }}</td></tr></table>' +
+          '</ul>'
+
+      };
+    });
 
 }());
